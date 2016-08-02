e63e180e05f45d137ca75c98e94dde716c887a5b
igor <igor.sv.n@gmail.com>
2016-08-02 10:44:11 +0300

    Join translations in builder.

--- Builder/Builder.php
+++ Builder/Builder.php
@@ -10,9 +10,11 @@
 
 namespace Darvin\MenuBundle\Builder;
 
+use Darvin\ContentBundle\Translatable\TranslationJoinerInterface;
 use Darvin\MenuBundle\Item\ItemFactoryInterface;
 use Darvin\MenuBundle\Repository\Menu\ItemRepository;
 use Darvin\Utils\CustomObject\CustomObjectLoaderInterface;
+use Doctrine\ORM\QueryBuilder;
 use Knp\Menu\FactoryInterface;
 
 /**
@@ -38,6 +40,11 @@ class Builder
     private $menuItemRepository;
 
     /**
+     * @var \Darvin\ContentBundle\Translatable\TranslationJoinerInterface
+     */
+    private $translationJoiner;
+
+    /**
      * @var string
      */
     private $menuAlias;
@@ -48,20 +55,23 @@ class Builder
     private $itemFactories;
 
     /**
-     * @param \Darvin\Utils\CustomObject\CustomObjectLoaderInterface $customObjectLoader Custom object loader
-     * @param \Knp\Menu\FactoryInterface                             $genericItemFactory Generic item factory
-     * @param \Darvin\MenuBundle\Repository\Menu\ItemRepository      $menuItemRepository Menu item entity repository
-     * @param string                                                 $menuAlias          Menu alias
+     * @param \Darvin\Utils\CustomObject\CustomObjectLoaderInterface        $customObjectLoader Custom object loader
+     * @param \Knp\Menu\FactoryInterface                                    $genericItemFactory Generic item factory
+     * @param \Darvin\MenuBundle\Repository\Menu\ItemRepository             $menuItemRepository Menu item entity repository
+     * @param \Darvin\ContentBundle\Translatable\TranslationJoinerInterface $translationJoiner  Translation joiner
+     * @param string                                                        $menuAlias          Menu alias
      */
     public function __construct(
         CustomObjectLoaderInterface $customObjectLoader,
         FactoryInterface $genericItemFactory,
         ItemRepository $menuItemRepository,
+        TranslationJoinerInterface $translationJoiner,
         $menuAlias
     ) {
         $this->customObjectLoader = $customObjectLoader;
         $this->genericItemFactory = $genericItemFactory;
         $this->menuItemRepository = $menuItemRepository;
+        $this->translationJoiner = $translationJoiner;
         $this->menuAlias = $menuAlias;
 
         $this->itemFactories = [];
@@ -86,8 +96,17 @@ class Builder
         foreach ($this->getMenuItems() as $menuItem) {
             $associated = $menuItem->getAssociatedInstance();
 
-            if (!empty($associated) && isset($this->itemFactories[$menuItem->getAssociatedClass()])) {
-                $root->addChild($this->itemFactories[$menuItem->getAssociatedClass()]->createItem($associated));
+            if (!empty($associated)
+                && isset($this->itemFactories[$menuItem->getAssociatedClass()])
+                && null !== $item = $this->itemFactories[$menuItem->getAssociatedClass()]->createItem($associated)
+            ) {
+                $title = $menuItem->getTitle();
+
+                if (!empty($title)) {
+                    $item->setName($title);
+                }
+
+                $root->addChild($item);
             }
         }
 
@@ -101,7 +120,13 @@ class Builder
     {
         $menuItems = $this->menuItemRepository->getByMenuEnabledBuilder($this->menuAlias)->getQuery()->getResult();
 
-        $this->customObjectLoader->loadCustomObjects($menuItems);
+        $translationJoiner = $this->translationJoiner;
+
+        $this->customObjectLoader->loadCustomObjects($menuItems, function (QueryBuilder $qb) use ($translationJoiner) {
+            if ($translationJoiner->isTranslatable($qb->getRootEntities()[0])) {
+                $translationJoiner->joinTranslation($qb, true, null, null, true);
+            }
+        });
 
         return $menuItems;
     }
