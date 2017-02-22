a27beaafab3f5684ebc01c12ba94286a05b5c013
igor <igor.sv.n@gmail.com>
2017-02-22 11:28:46 +0300

    Add KNP menu bundle app config.

--- DependencyInjection/DarvinMenuExtension.php
+++ DependencyInjection/DarvinMenuExtension.php
@@ -56,6 +56,7 @@ class DarvinMenuExtension extends Extension implements PrependExtensionInterface
 
         foreach ([
             'darvin_admin',
+            'knp_menu',
         ] as $extension) {
             if ($container->hasExtension($extension)) {
                 $container->prependExtensionConfig($extension, Yaml::parse(file_get_contents($fileLocator->locate($extension.'.yml')))[$extension]);
