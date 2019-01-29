c8aac9f6b663d16f885036aa4856bcc1cc52140e
Igor Nikolaev <igor.sv.n@gmail.com>
2019-01-29 16:36:49 +0300

    Enable strict types.

--- Form/DataTransformer/Admin/SlugMapItemToArrayTransformer.php
+++ Form/DataTransformer/Admin/SlugMapItemToArrayTransformer.php
@@ -1,7 +1,7 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * @author    Igor Nikolaev <igor.sv.n@gmail.com>
- * @copyright Copyright (c) 2017, Darvin Studio
+ * @copyright Copyright (c) 2017-2019, Darvin Studio
  * @link      https://www.darvin-studio.ru
  *
  * For the full copyright and license information, please view the LICENSE
@@ -36,7 +36,7 @@ class SlugMapItemToArrayTransformer implements DataTransformerInterface
     /**
      * {@inheritdoc}
      */
-    public function transform($value)
+    public function transform($value): ?array
     {
         if (null === $value) {
             return null;
@@ -56,7 +56,7 @@ class SlugMapItemToArrayTransformer implements DataTransformerInterface
     /**
      * {@inheritdoc}
      */
-    public function reverseTransform($value)
+    public function reverseTransform($value): ?SlugMapItem
     {
         $classProperty = $value['class_property'];
 
